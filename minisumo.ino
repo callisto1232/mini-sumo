cc640f7d6cd659341967062850d53ce63e3c5682
callisto1232 <146484672+callisto1232@users.noreply.github.com>
2025-05-15 19:42:51 +0300

    Update minisumo.ino

--- minisumo.ino
+++ minisumo.ino
@@ -1,170 +1,248 @@
 #include <NewPing.h>
 
-#define TRIG_PIN_1 6
-#define ECHO_PIN_1 7
-#define TRIG_PIN_2 8
-#define ECHO_PIN_2 9
-#define TRIG_PIN_3 10
-#define ECHO_PIN_3 11
+// Pin definitions
+#define TRIG_PIN_1 4
+#define ECHO_PIN_1 5
+#define TRIG_PIN_2 2
+#define ECHO_PIN_2 3
+#define TRIG_PIN_3 6
+#define ECHO_PIN_3 7
 #define MAX_DISTANCE 200
+
 #define INF_FRONT A0
 #define INF_BACK A1
-#define A1A 4
-#define A1B 5
-#define B1A 2
-#define B1B 3
-#define ENA A2 //to control speed
-#define ENB A3 // so
-int speedA  = 255; // 0-255, 255 max
+
+#define A1A 8
+#define A1B 10
+#define B1A 12
+#define B1B 14
+#define ENA A2 // Speed control for motor A
+#define ENB A3 // Speed control for motor B
+
+int speedA = 255; // Max speed (0-255)
 int speedB = 255;
-const int inf_thresold = 700; // example While: 400, Black: 900, good thresold: 650
-string enemyPosition;
-sensors = [0, 0, 0, 0, 0]  // inf back, inf front, sonar left, sonar front, sonar right
+
+const int threshold = 700; // Infrared sensor threshold
+
+// Sensors array: [inf_back, inf_front, sonar_left, sonar_front, sonar_right]
+int sensors[5] = {0, 0, 0, 0, 0};
+
+String enemyPosition = "none";
+
 unsigned long previousMillis = 0;
 const long interval = 50; // 50 ms between sensor readings
 
-NewPing sonar1(TRIG_PIN_1, ECHO_PIN_1, MAX_DISTANCE);
-NewPing sonar2(TRIG_PIN_2, ECHO_PIN_2, MAX_DISTANCE);
-NewPing sonar3(TRIG_PIN_3, ECHO_PIN_3, MAX_DISTANCE);
+// Initialize sonar sensors
+NewPing sonarLeft(TRIG_PIN_1, ECHO_PIN_1, MAX_DISTANCE);
+NewPing sonarFront(TRIG_PIN_2, ECHO_PIN_2, MAX_DISTANCE);
+NewPing sonarRight(TRIG_PIN_3, ECHO_PIN_3, MAX_DISTANCE);
 
-
-
-void setup(){
+void setup() {
   Serial.begin(9600);
+
   pinMode(INF_FRONT, INPUT);
   pinMode(INF_BACK, INPUT);
+
   pinMode(ENA, OUTPUT);
   pinMode(ENB, OUTPUT);
   pinMode(A1A, OUTPUT);
   pinMode(A1B, OUTPUT);
   pinMode(B1A, OUTPUT);
-  pinMode(B1B, OUTPUT);      
+  pinMode(B1B, OUTPUT);
+
+  stopMotors();
 }
 
 void loop() {
-  inf_sensors()
-  if enemyPosition == "left" {
-    moveLeft();
-  } else if enemyPosition == "left_front" {
-    moveForwardLeft();
-  } else if enemyPosition == "front" {
-    moveForward();
-    // we need to use delay here but we won't. last comment 
-  } else if enemyPosition == "right_front" {
-    moveForwardRight();
-  } else if enemyPosition == "right" {
-    turnRight();
-  } else if enemyPosition == "none" {
-    //check again then turn around (left)
-    turnLeft();
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+
+    readSensors();
+    determineEnemyPosition();
+
+    // Avoid leaving dohyo
+    if (sensors[0] == 1) { // Infrared detects white line
+      stopMotors();
+      moveBackwardShort();
+      turnLeftShort();
+      stopMotors();
+      return;
+    }
+    if (sensors[1] == 1){ // öndeki de arkadikde aynısını yapamaz
+      stopMotors();
+      moveForwardShort();
+      turnLeftShort();
+      stopMotors();
+      return;
+    }
+
+    // Movement based on enemy position
+    if (enemyPosition == "left") {
+      moveLeft();
+    } else if (enemyPosition == "left_front") {
+      moveForwardLeft();
+    } else if (enemyPosition == "front") {
+      moveForward();
+    } else if (enemyPosition == "right_front") {
+      moveForwardRight();
+    } else if (enemyPosition == "right") {
+      turnRight();
+    } else {
+      // No enemy detected, search by turning left
+      turnLeft();
+    }
   }
-  
 }
 
+// Movement functions
 void moveForward() {
+  analogWrite(ENA, speedA);
+  analogWrite(ENB, speedB);
+
   analogWrite(A1A, 0);
   analogWrite(A1B, speedA);
+
   analogWrite(B1A, 0);
   analogWrite(B1B, speedB);
 }
 
 void moveBackward() {
+  analogWrite(ENA, speedA);
+  analogWrite(ENB, speedB);
+
   analogWrite(A1A, speedA);
   analogWrite(A1B, 0);
+
   analogWrite(B1A, speedB);
   analogWrite(B1B, 0);
+}
+
+void moveBackwardShort() {
+  moveBackward();
+  delay(200);
+  stopMotors();
+}
+
+void moveForwardShort() {
+  moveForward();
+  delay(200);
+  stopMotors();
+}
+
+void moveLeft() {
+  analogWrite(ENA, speedA);
+  analogWrite(ENB, speedB);
+
+  analogWrite(A1A, 0);
+  analogWrite(A1B, speedA);
+
+  analogWrite(B1A, speedB);
+  analogWrite(B1B, 0);
+}
+
+void moveForwardLeft() {
+  analogWrite(ENA, speedA);
+  analogWrite(ENB, speedB);
+
+  analogWrite(A1A, 0);
+  analogWrite(A1B, speedA);
+
+  analogWrite(B1A, 0);
+  analogWrite(B1B, speedB / 2);
 }
 
 void moveForwardRight() {
+  analogWrite(ENA, speedA);
+  analogWrite(ENB, speedB);
+
   analogWrite(A1A, 0);
-  analogWrite(A1B, speedA);  
+  analogWrite(A1B, speedA);
+
   analogWrite(B1A, 0);
-  analogWrite(B1B, speedB/2);
+  analogWrite(B1B, speedB / 2);
 }
 
 void turnLeft() {
+  analogWrite(ENA, speedA);
+  analogWrite(ENB, speedB);
+
   analogWrite(A1A, 0);
-  analogWrite(A1B, speedA); 
+  analogWrite(A1B, speedA);
+
   analogWrite(B1A, speedB);
-  analogWrite(B1B, 0);   
+  analogWrite(B1B, 0);
+}
+
+void turnLeftShort() {
+  turnLeft();
+  delay(300);
+  stopMotors();
 }
 
 void turnRight() {
+  analogWrite(ENA, speedA);
+  analogWrite(ENB, speedB);
+
   analogWrite(A1A, speedA);
-  analogWrite(A1B, 0);  
+  analogWrite(A1B, 0);
+
   analogWrite(B1A, 0);
-  analogWrite(B1B, speedB); 
+  analogWrite(B1B, speedB);
 }
 
 void stopMotors() {
+  analogWrite(ENA, 0);
+  analogWrite(ENB, 0);
+
   analogWrite(A1A, 0);
   analogWrite(A1B, 0);
+
   analogWrite(B1A, 0);
   analogWrite(B1B, 0);
 }
 
-
-void distance_far(int distance) { // is it 255 when no object? or blocked view
-  if (0 < distance <= 20 ) {
-    return 1;
-  } else if (distance > 20 && distance <= 40){
-    return 2; 
-  } else if (distance > 40){
-    return 3;
-  } else {
-    return 0;
-  }
-
+// Convert distance to category
+int distance_far(int distance) {
+  if (distance > 0 && distance <= 20) return 1;
+  else if (distance > 20 && distance <= 40) return 2;
+  else if (distance > 40) return 3;
+  else return 0;
 }
 
-void inf_sensors() {
-  bool frontWhite = analogRead(INF_FRONT) < threshold;
-  bool backWhite = analogRead(INF_BACK) < threshold;
-  sensors[0] = frontWhite;
-  sensors[1] = backWhite;
+// Read sensors
+void readSensors() {
+  sensors[0] = (analogRead(INF_BACK) > threshold) ? 1 : 0;
+  sensors[1] = (analogRead(INF_FRONT) > threshold) ? 1 : 0;
+
   sensors[2] = distance_far(sonarLeft.ping_cm());
   sensors[3] = distance_far(sonarFront.ping_cm());
   sensors[4] = distance_far(sonarRight.ping_cm());
+
+  // Debug print
+  Serial.print("IR Back: "); Serial.print(sensors[0]);
+  Serial.print(", IR Front: "); Serial.print(sensors[1]);
+  Serial.print(", Sonar Left: "); Serial.print(sensors[2]);
+  Serial.print(", Sonar Front: "); Serial.print(sensors[3]);
+  Serial.print(", Sonar Right: "); Serial.println(sensors[4]);
 }
 
-// sensors[2] left distance, sensors[3] front distance, sensors[4] right distance
-void enemyPosition(){
-  if sensors[2] == 1 && sensors[3] == 3 && sensors[4] == 3{
+// Determine enemy position
+void determineEnemyPosition() {
+  if (sensors[2] == 1 && sensors[3] == 3 && sensors[4] == 3) {
     enemyPosition = "left";
-  } else if sensors[2] == 1 && sensors[3] == 2 && sensors[4] == 3{
+  } else if (sensors[2] == 1 && sensors[3] == 2 && sensors[4] == 3) {
     enemyPosition = "left_front";
-  } else if sensors[2] == 3 && sensors[3] == 1 && sensors[4] == 3{
+  } else if (sensors[2] == 3 && sensors[3] == 1 && sensors[4] == 3) {
     enemyPosition = "front";
-  } else if sensors[2] == 3 && sensors[3] == 1 && sensors[4] == 2{
+  } else if (sensors[2] == 3 && sensors[3] == 1 && sensors[4] == 2) {
     enemyPosition = "right_front";
-  } else if sensors[2] == 3 && sensors[3] == 3 && sensors[4] == 1{
+  } else if (sensors[2] == 3 && sensors[3] == 3 && sensors[4] == 1) {
     enemyPosition = "right";
   } else {
     enemyPosition = "none";
   }
 
+  Serial.print("Enemy Position: ");
+  Serial.println(enemyPosition);
 }
-// add if else to control at the beginning of the loop function to check if infrared sensors are white not to go out of the dohyo
-// qwertyuiop
-// we don't actually need to check infrared ones when distance is detected, unless the enemy escapes and we go out of the dohyo
-// in moving functions, if we just move the robot a little bit then check for the infrared again, we may reduce the possibility to exit the dohyo
-// we can collect the data to an array then check the average as in the github mini sumo repo by ethanphunter
-
-/*
-else if (lineR == LOW && lineL == HIGH) { // Right line sensor hits the white line and triggers low
-  motors(-100, -100);           // Back up
-  delay(200);                  // Only back up for .2 seconds
-  end_time = millis() + 400;   // Find the time 400 miliseconds into the future
-  while(millis() < end_time) { // Spin away from edge untill the time limit is hit
-    motors(-100, 100);
-    sensors();
-    if(midL > threshold || midR > threshold || prox == LOW){  // If you see a target, break loop
-      break;
-    }
-    digitalWrite(LED1_pin, LOW);
-    digitalWrite(LED2_pin, LOW);
-  }
-}
-
-it is using time to check when to move, where to move rather than delay, this way you can both check and move at the same time
-*/
